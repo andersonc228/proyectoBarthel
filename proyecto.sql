2fff4370487015d5eb9aa7c24d67a6a6c2ed85d8
andersonc228 <andersonc228@gmail.com>
2019-05-26 22:18:51 +0200

    data base

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2019 a las 20:17:56
+-- Tiempo de generación: 26-05-2019 a las 21:52:20
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.3.1
 
@@ -21,6 +21,32 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyecto`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `dateAppointment` date NOT NULL,
+  `dniPacient` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `dateAppointment`, `dniPacient`) VALUES
+(1, '2019-05-19', '123'),
+(3, '2019-05-12', '123'),
+(4, '2019-05-24', '123'),
+(7, '2019-05-05', '123'),
+(8, '2019-05-05', '123'),
+(10, '2019-05-05', '123'),
+(11, '2019-05-25', '123'),
+(12, '2019-05-25', '11111111H');
 
 -- --------------------------------------------------------
 
@@ -57,6 +83,13 @@ CREATE TABLE `password_resets` (
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('ssss@gmail.com', '$2y$10$qpBy9SVXlo0krkHQwNMFTuJh1OVzk0pK6haLY9WawD/h34Gi9GJ.G', '2019-05-15 12:54:26');
+
 -- --------------------------------------------------------
 
 --
@@ -81,6 +114,14 @@ CREATE TABLE `reports` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `reports`
+--
+
+INSERT INTO `reports` (`id`, `q1`, `q2`, `q3`, `q4`, `q5`, `q6`, `q7`, `q8`, `q9`, `q10`, `total`, `dniPacient`, `created_at`, `updated_at`) VALUES
+(2, 0, 5, 5, 12, 22, 0, 5, 52, 52, 52, 100222, '11111111H', '2019-05-22 11:51:22', '2019-05-22 11:51:22'),
+(7, 0, 5, 5, 5, 5, 0, 5, 5, 5, 5, 100, '11111111H', '2019-05-22 11:51:22', '2019-05-22 11:51:22');
+
 -- --------------------------------------------------------
 
 --
@@ -100,9 +141,9 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'Administrator', '2019-05-13 11:33:38', '2019-05-13 11:33:38'),
-(2, 'pacient', 'Pacient', '2019-05-13 11:33:38', '2019-05-13 11:33:38'),
-(3, 'doctor', 'Doctor', '2019-05-13 11:33:38', '2019-05-13 11:33:38');
+(1, 'admin', 'Administrator', '2019-05-13 09:33:38', '2019-05-13 09:33:38'),
+(2, 'pacient', 'Pacient', '2019-05-13 09:33:38', '2019-05-13 09:33:38'),
+(3, 'doctor', 'Doctor', '2019-05-13 09:33:38', '2019-05-13 09:33:38');
 
 -- --------------------------------------------------------
 
@@ -123,8 +164,13 @@ CREATE TABLE `role_user` (
 --
 
 INSERT INTO `role_user` (`id`, `role_id`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 1, 14, '2019-05-13 15:00:41', '2019-05-13 15:00:41'),
-(2, 2, 16, '2019-05-14 03:39:14', '2019-05-14 03:39:14');
+(1, 1, 19, '2019-05-13 13:00:41', '2019-05-13 13:00:41'),
+(2, 2, 16, '2019-05-14 01:39:14', '2019-05-14 01:39:14'),
+(3, 2, 20, '2019-05-15 11:41:34', '2019-05-15 11:41:34'),
+(4, 3, 17, '2019-05-15 11:45:22', '2019-05-15 11:45:22'),
+(18, 2, 43, '2019-05-21 14:22:32', '2019-05-21 14:22:32'),
+(19, 2, 47, '2019-05-22 01:35:33', '2019-05-22 01:35:33'),
+(20, 2, 37, '2019-05-17 10:13:06', '2019-05-17 10:13:06');
 
 -- --------------------------------------------------------
 
@@ -139,6 +185,7 @@ CREATE TABLE `users` (
   `surname` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `bornDate` datetime NOT NULL,
   `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` int(9) NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `profesion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -150,23 +197,23 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `dni`, `name`, `surname`, `bornDate`, `email`, `password`, `profesion`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, '1231321a', 'Diego Carretero', 'pepito', '2019-06-08 00:00:00', 'anderson@gmail.com', '$2y$10$I5VuaZB6e32yQllDTZRGMegL480iElRUiZiX3i9lOAxiS9XaCYTYe', NULL, NULL, '2019-05-13 12:33:20', '2019-05-13 12:33:20'),
-(4, '4560839w', 'admin', 'pepito', '2019-05-16 00:00:00', 'user@example.com', '$2y$10$5fAGiHAsqYKs4f2lxKY/T.bXIAY3MnWydbocWz0qt0BNEwvFjUlJm', NULL, NULL, '2019-05-13 12:34:56', '2019-05-13 12:34:56'),
-(5, '45608392d', 'ssaaaaaa', 'pepito rojas', '2019-05-09 00:00:00', 'ooooo@oooo.com', '$2y$10$El11hT6/3/zVFsq4koqIuuij8AObyBYjEwfowRCxIzAXsQXO02/I.', NULL, NULL, '2019-05-13 12:37:58', '2019-05-13 12:37:58'),
-(6, '4560839r', 'rojas', 'pepito', '2019-05-02 00:00:00', 'peter13@pepito.com', '$2y$10$/Rm.uX0E2Mj43SGviDgz1e5cja.x..dPXTB2mFPx306NE.jHCIv2e', NULL, NULL, '2019-05-13 12:38:50', '2019-05-13 12:38:50'),
-(7, '1231321t', 'Anderson', 'jajajajajjaja', '2019-05-04 00:00:00', 'peters1@pepito.com', '$2y$10$0JvzTS/chymHf31kSOl7Lew1kV9a0XruQwoTuuw5h2TCFP0EqXCRu', NULL, NULL, '2019-05-13 12:42:39', '2019-05-13 12:42:39'),
-(8, '1231321x', 'prueb', 'jajajajajjaja', '2019-05-09 00:00:00', 'userss@example.com', '$2y$10$Sj8H1Tyb2N42WbCLLDnzWuGE87CFEQLyJLO.4zwWwRVU.9.QPLmfi', NULL, NULL, '2019-05-13 12:44:46', '2019-05-13 12:44:46'),
-(9, '1231321q', 'Anderson', 'pepito rojas', '2019-05-11 00:00:00', 'andersonwww@gmail.com', '$2y$10$bPcPl2fjT4UGx9qkzWN9cuBIDtgoS2lb6fwmSLZ6f1KndW1Z7f/EC', NULL, NULL, '2019-05-13 12:47:01', '2019-05-13 12:47:01'),
-(10, '1231321y', 'Anderson', 'jajajajajjaja', '2019-05-02 00:00:00', 'peter144@pepito.com', '$2y$10$pHotER4b0x8EMq.sCVMFJuFfTL6mpobMfEYtUnmXPvxY2g9pE1LI2', NULL, NULL, '2019-05-13 13:01:50', '2019-05-13 13:01:50'),
-(11, '1231321ee', 'prueb', 'pepito', '2019-05-08 00:00:00', 'peter133@pepito.com', '$2y$10$H6sMCr60LHCCzP476VNmJez1gC1bzXjttrOONTDwUcZ9vW3HNCtqC', NULL, NULL, '2019-05-13 13:02:40', '2019-05-13 13:02:40'),
-(12, '1222ddd', 'aassss', 'asssss', '2019-05-09 00:00:00', 'peter333@pepito.com', '$2y$10$SvKEHZZkHcfez.g4/9xvdOm0zOcHvtaPBhwJSTRDfCeJVnVM.70.K', NULL, NULL, '2019-05-13 13:27:59', '2019-05-13 13:27:59'),
-(14, 'anderson', 'anderson', 'anderson', '9222-02-09 00:00:00', 'andersonc228@gmail.com', '$2y$10$mB490iH6/Jzf1SV40gaKGuxmhOOVcbXAfZsXgQahSlBQdc7wfLMOG', NULL, NULL, '2019-05-13 15:00:41', '2019-05-13 15:00:41'),
-(16, '12345', 'aasdas', 'dsada', '2019-05-15 00:00:00', 'andersonc@gmail.com', '$2y$10$MWLHU7b8E2J7XzAsSQGq8eT9VBSXREm5H/sXv0uPau./1gl/FgrGy', NULL, NULL, '2019-05-14 03:39:14', '2019-05-14 03:39:14');
+INSERT INTO `users` (`id`, `dni`, `name`, `surname`, `bornDate`, `email`, `phone`, `password`, `profesion`, `remember_token`, `created_at`, `updated_at`) VALUES
+(17, '4560839e', 'coronado', 'rojas', '2900-09-29 00:00:00', 'andersonco@hotmail.com', 0, '$2y$10$EYP.HaO8L.oR7jOLJy/OHehoFBjN9kP1BT/aVR2o8KIBwaXFqyTfG', NULL, NULL, '2019-05-15 11:41:34', '2019-05-15 11:41:34'),
+(19, 'y4560839e', 'anderson', 'coronado', '1992-09-28 00:00:00', 'rojas@gmail.com', 0, '$2y$10$EYP.HaO8L.oR7jOLJy/OHehoFBjN9kP1BT/aVR2o8KIBwaXFqyTfG', NULL, NULL, '2019-05-15 11:45:22', '2019-05-21 12:05:02'),
+(20, '123', 'ssss', 'ssss', '2029-09-28 00:00:00', 'ssss@gmail.com', 9000000, '$2y$10$1peIsWI6lNfbaGBmf5bh1eXf8fIihAgJgGxxvPB76TvjNTtjwrk46', NULL, NULL, '2019-05-15 11:46:19', '2019-05-15 11:46:19'),
+(37, '4560839ea', '27', 'pepito', '2019-05-31 00:00:00', 'peter1@pepito.com', 0, '$2y$10$1peIsWI6lNfbaGBmf5bh1eXf8fIihAgJgGxxvPB76TvjNTtjwrk46', NULL, NULL, '2019-05-17 10:13:06', '2019-05-17 10:13:06'),
+(43, '11111111H', 'Anderson', 'coronado', '1992-06-28 00:00:00', 'coronado@gmail.com', 635867128, '$2y$10$gpykYCP4Or5dujBLVrKFy.aoMIbRNtf6ZqoT.dcCXjKxzdL6WCrKa', NULL, NULL, '2019-05-21 14:22:32', '2019-05-21 14:23:24');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `appointments_dnipacient_foreign` (`dniPacient`);
 
 --
 -- Indices de la tabla `migrations`
@@ -212,6 +259,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
@@ -221,7 +274,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT de la tabla `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -233,23 +286,29 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `role_user`
 --
 ALTER TABLE `role_user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_dnipacient_foreign` FOREIGN KEY (`dniPacient`) REFERENCES `users` (`dni`) ON DELETE CASCADE;
+
+--
 -- Filtros para la tabla `reports`
 --
 ALTER TABLE `reports`
-  ADD CONSTRAINT `reports_dnipacient_foreign` FOREIGN KEY (`dniPacient`) REFERENCES `users` (`dni`);
+  ADD CONSTRAINT `reports_dnipacient_foreign` FOREIGN KEY (`dniPacient`) REFERENCES `users` (`dni`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
