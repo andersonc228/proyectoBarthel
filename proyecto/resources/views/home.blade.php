7b04ccd0c245df31e61973b034da80efa6d7b6e0
juandavid92 <juanda02031992@gmail.com>
2019-05-28 20:12:46 +0200

    home edit

--- proyecto/resources/views/home.blade.php
+++ proyecto/resources/views/home.blade.php
@@ -9,8 +9,18 @@
         @section('content')
         <div class="col-md-8">
             <div class="card">
-                @if(null!=Auth::user())
                 <div class="card-header">Welcome {{ Auth::user()->name }}</div>
+                @if(Auth::user()->hasRole('admin'))
+                <div class="row row-home">
+                    <div class="col-md-4 doctor-registrer btns"><a href="{{ route('registerDoctor') }}">REGISTER DOCTOR</a></div>
+                    <div class="col-md-4 pacient-registrer btns"><a href="{{ route('registerPacient') }}">REGISTER PACIENT</a></div>
+                    <div class="col-md-4 admin-registrer btns"><a href="{{ route('registerAdministrator') }}">REGISTER ADMINISTRATOR</a></div>
+                </div>
+                <div class="row row-home">
+                    <div class="col-md-6 doctor-registrer btns">FIND USER</div>
+                    <div class="col-md-6 pacient-registrer btns">UPDATE PASSWORD</div>
+                </div>
+                @endif
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -23,7 +33,6 @@
                     @endif
                     You are logged in!
                 </div>
-                @endif
             </div>
         </div>
     </div>
